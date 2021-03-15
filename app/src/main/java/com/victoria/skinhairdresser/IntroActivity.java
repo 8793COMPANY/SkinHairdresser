2ff1544df464abebeefbcef1ac78e8ca06adbde4
gyeong <gyeongyeongi22@gmail.com>
2021-03-15 16:59:58 +0900

    03/15 레이아웃 작업 완료

--- app/src/main/java/com/victoria/skinhairdresser/IntroActivity.java
+++ app/src/main/java/com/victoria/skinhairdresser/IntroActivity.java
@@ -3,35 +3,56 @@ package com.victoria.skinhairdresser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
+
+import java.util.Locale;
 
 public class IntroActivity extends AppCompatActivity {
     //인트로
-
+    int count = 0;
+    ProgressBar progressBar;
     Handler handler = new Handler();
     Runnable r = new Runnable() {
         @Override
         public void run() {
-            Intent intent = new Intent(getApplicationContext(), IntroActivity2.class);
+            Intent intent = new Intent(getApplicationContext(), LanguageActivity.class);
             startActivity(intent);
             finish();
         }
     };
 
+    Runnable progress_start = new Runnable() {
+        @Override
+        public void run() {
+            if (count == 300){
+                handler.removeCallbacksAndMessages(null);
+                Intent intent = new Intent(getApplicationContext(), LanguageActivity.class);
+                startActivity(intent);
+                finish();
+            }else{
+                count++;
+                progressBar.setProgress(count);
+                handler.postDelayed(progress_start,10);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
-
+        progressBar = findViewById(R.id.progress);
+        progressBar.setMax(300);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        handler.postDelayed(r, 4000); // 4초 뒤에 Runnable 객체 수행
+//        handler.postDelayed(r, 3000); // 4초 뒤에 Runnable 객체 수행
+        handler.post(progress_start);
     }
 
     @Override
--- app/src/main/java/com/victoria/skinhairdresser/LoginActivity.java
+++ app/src/main/java/com/victoria/skinhairdresser/LoginActivity.java
@@ -4,16 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.storage.OnObbStateChangeListener;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     //로그인
+
+    Button login_btn,join_btn;
+    EditText model_number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
 
+        login_btn = findViewById(R.id.login_btn);
+        join_btn = findViewById(R.id.join_btn);
+        model_number = findViewById(R.id.model_num);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (model_number.getText().toString().trim().equals("")){
+                    Toast.makeText(getApplicationContext(),"모델번호를 입력해주세요.",Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
     }
 }
