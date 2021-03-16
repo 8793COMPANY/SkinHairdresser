e545a055f092363a0116c7a9e0e99b83f084167b
Doo-Dong <ehd6754@gmail.com>
2021-03-16 16:53:28 +0900

    1. 측정기 버그 수정 2. 레이아웃 일부 수정 3. 사용자 피드백 추가

--- app/src/main/java/com/victoria/skinhairdresser/StartActivity.java
+++ app/src/main/java/com/victoria/skinhairdresser/StartActivity.java
@@ -5,10 +5,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +21,9 @@ import android.widget.Toast;
 
 import com.victoria.skinhairdresser.cw.CustomWebView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import at.grabner.circleprogress.CircleProgressView;
 
 import static android.Manifest.permission.INTERNET;
@@ -27,6 +32,7 @@ public class StartActivity extends AppCompatActivity {
     // global
     // TODO : 주소 수정 필요 !!
     final String HOST = "http://192.168.4.1";
+    Map<String, String> extraHeaders = new HashMap<String, String>();
     private final int PERMISSIONS_REQUEST_RESULT = 1;
 
     // cw
@@ -58,6 +64,8 @@ public class StartActivity extends AppCompatActivity {
 
         // local
         wv = findViewById(R.id.wv);
+        wv.setScaleX(1.5f);
+        wv.setScaleY(1.5f);
         progress_circular = findViewById(R.id.progress_circular);
         progress_circular.setMaxValue(second);
         btn_stream = findViewById(R.id.start_stream_btn);
@@ -69,6 +77,8 @@ public class StartActivity extends AppCompatActivity {
 
         final Handler handler = new Handler();
 
+        final Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
         start_close_btn.setOnClickListener(v -> {
             finish();
         });
@@ -77,13 +87,22 @@ public class StartActivity extends AppCompatActivity {
             if(v.isSelected()) {
                 wv.stopLoading();
                 wv.loadUrl("about:blank");
+                start_tv_1.setText("스트리밍 대기 중 ···");
+                start_tv_2.setText("스트리밍 시작하기 버튼을 눌러주세요");
+                v.setSelected(false);
             } else {
                 PlayHttpStream(HOST + ":81/stream");
+                start_tv_1.setText("측정 준비 완료");
+                start_tv_2.setText("측정하기 버튼을 눌러주세요");
                 v.setSelected(true);
             }
         });
 
         start_20_sec.setOnClickListener(v -> {
+            start_20_sec.setEnabled(false);
+            btn_stream.setEnabled(false);
+            start_tv_1.setText("첫 번째 측정 중 ···");
+            start_tv_2.setText("측정이 완료될 때까지 피부에서 떼지 마세요");
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
@@ -94,11 +113,12 @@ public class StartActivity extends AppCompatActivity {
                         Log.e("progress_circular", "progress: " + prg);
                         progress_circular.setValue(prg);
                     } else {
-                        Toast.makeText(StartActivity.this, "측정이 완료되었습니다", Toast.LENGTH_SHORT).show();
+                        vibrator.vibrate(100);
+                        Toast.makeText(StartActivity.this, "측정이 완료되었습니다", Toast.LENGTH_LONG).show();
 
                         Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
                         startActivity(intent);
-                        finish();
+                        recreate();
                     }
                 }
             },1000);
@@ -110,7 +130,9 @@ public class StartActivity extends AppCompatActivity {
 
     //play http stream
     private void PlayHttpStream(String httpUrl){
+        extraHeaders.put("X-Requested-With", "doodong.me");
+
         wv.getSettings().setJavaScriptEnabled(true);
-        wv.loadUrl(httpUrl);
+        wv.loadUrl(httpUrl, extraHeaders);
     }
 }
